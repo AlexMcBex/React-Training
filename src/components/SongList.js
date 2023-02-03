f6c69bba8bdefbddf4d3a3daafcc5c02b4837cae
AlexMcBex <alessiobelli94@gmail.com>
2023-02-03 11:34:26 -0500

    finished music lesson

---
+++ src/components/SongList.js
@@ -0,0 +1,16 @@
+const SongList = (props)=>{
+    console.log(props.handleClick)
+    return(
+        <>
+        {props.songs.map(song => {
+            return (
+              <>
+                <h3>{song}</h3>
+              </>
+            )
+          })}
+          </>
+    )
+}
+
+export default SongList
